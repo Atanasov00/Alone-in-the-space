48f8e0866ee6fd5004860d38e33ee44b5870c512
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-12-16 12:40:54 +0100

    add spawnAsteroids

--- src/main/java/model/asteroid/AsteroidFactory.java
+++ src/main/java/model/asteroid/AsteroidFactory.java
@@ -1,8 +1,13 @@
 package model.asteroid;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import com.almasb.fxgl.core.math.Vec2;
 
 import javafx.scene.image.ImageView;
+import utilities.EnumInt;
 
 public class AsteroidFactory {
 	
@@ -12,5 +17,23 @@ public class AsteroidFactory {
 	
 	public static UnbreakableAsteroid unbreakableAsteroid(final Vec2 pos, final ImageView image) {
 		return new UnbreakableAsteroid(pos, image);
+	}
+	
+	public static Set<Asteroid> spawnAsteroids(){
+		Set<BasicAsteroid> basic = new HashSet<>();
+		Set<UnbreakableAsteroid> unbreakable = new HashSet<>();
+		Random rnd = new Random();
+		
+		while(basic.size() == 0) {
+			if(basic.size() == 0) {
+				basic.add(basicAsteroid(new Vec2(rnd.nextInt(EnumInt.WIDTH.getValue()), rnd.nextInt(EnumInt.HEIGHT.getValue())), 
+						100, new ImageView("images/asteroid_02.png")));
+			}
+		}
+		
+		Set<Asteroid> asteroid = new HashSet<>();
+		asteroid.addAll(basic);
+		
+		return asteroid;
 	}
 }
