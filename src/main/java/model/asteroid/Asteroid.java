e19af2617505fe46f352b53fedb8bfba3a08aee6
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-12-16 11:27:03 +0100

    develop Asteroid

--- src/main/java/model/asteroid/Asteroid.java
+++ src/main/java/model/asteroid/Asteroid.java
@@ -1,5 +1,14 @@
 package model.asteroid;
 
-public interface Asteroid {
+import com.almasb.fxgl.core.math.Vec2;
 
+import javafx.scene.image.ImageView;
+
+public interface Asteroid {
+	
+	Vec2 getPos();
+	
+	void setPos(Vec2 pos);
+	
+	ImageView getImage();
 }
